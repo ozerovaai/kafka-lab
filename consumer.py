485e869feef37afbdb67b36e5d193ba5723608ef
ozerovaai <ozerova_a_i@my.mgimo.ru>
2026-03-15 21:33:17 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,33 +2,27 @@ from kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer(
-    "library",
+    "carsharing",
     bootstrap_servers="localhost:9092",
     value_deserializer=lambda m: m.decode("utf-8")
 )
 
 def validate(data):
-    required_fields = ["user", "book", "action", "time"]
-
+    required_fields = ["user", "car", "action", "time"]
     for field in required_fields:
         if field not in data:
             return False
-
-    if data["action"] not in ["borrow", "return"]:
+    if data["action"] not in ["start_ride", "end_ride"]:
         return False
-
     return True
 
 for message in consumer:
     msg = message.value
-
     try:
         data = json.loads(msg)
-
         if validate(data):
             print("VALID:", data)
         else:
             print("NOT VALID:", msg)
-
-    except:
+    except Exception:
         print("NOT VALID:", msg)
